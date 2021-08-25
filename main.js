7faf7c38e2ed87fa03764867cfc267f1768d716f
Alex Wower <woweralexander@gmail.com>
2021-08-25 14:16:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// Random number generator to pull which phrase will be on screen
+
+//depending on which random number, different grid?array will be input (phrase). will need separate styling 
+
+//operator to have 6 guess. must receive inputs via keyboard input
+
+// unhide numbers if correctly guessed.
+// incorrect guessed, add to list display and add to counter
+// if all letters have been correctly guessed, 
+
+
+
+
+
+//timer to count down to zero
+//randomly set a button to active (show mole)
+//log a point when mole is clicked and deactivate button
+//// when timer is out, display score
+
+
+
+
+// 
